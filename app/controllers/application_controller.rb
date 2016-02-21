f7b3a1175a7945c56c0804a1fa32ec763f2a1dbc
cherriepie <ibchengyu@gmail.com>
2016-02-20 18:21:51 -0800

    interim

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -7,6 +7,11 @@ class ApplicationController < ActionController::Base
   
   before_action :authenticate_user!
 
+  # Overwriting the sign_out redirect path method
+  def after_sign_out_path_for(resource_or_scope)
+    root_path
+  end
+
   layout :layout_by_resource
 
   protected
